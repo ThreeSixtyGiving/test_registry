ca179c90c3dde62023ddc89ea037a936f4061f3c
Ben Webb <ben.webb@opendataservices.coop>
2018-01-17 15:30:43 +0000

    Use --no-convert-big-files option to convert smaller files

--- test_datagetter.sh
+++ test_datagetter.sh
@@ -4,8 +4,8 @@ git clone https://github.com/ThreeSixtyGiving/datagetter.git
 cd datagetter
 pip install -r requirements.txt
 mkdir -p data/{original,json_all,json_valid,json_acceptable_license,json_acceptable_license_valid}
-# Don't convert because Travis probably doesn't have the RAM
-python get.py --no-convert 
+# Don't convert big file becasue Travis doesn't have the RAM
+python get.py --no-convert-big-files
 exit_status=$?
 python report.py
 echo $GIST_TOKEN > ~/.gist
