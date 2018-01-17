c66b56f9918af687426ebfd67213099beb650965
Ben Webb <ben.webb@opendataservices.coop>
2018-01-17 10:44:50 +0000

    Upload csv report to GitHub Gist

--- test_datagetter.sh
+++ test_datagetter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 rm -rf datagetter || true
 git clone https://github.com/ThreeSixtyGiving/datagetter.git
 cd datagetter
@@ -7,3 +6,8 @@ pip install -r requirements.txt
 mkdir -p data/{original,json_all,json_valid,json_acceptable_license,json_acceptable_license_valid}
 # Don't convert because Travis probably doesn't have the RAM
 python get.py --no-convert 
+exit_status=$?
+python report.py
+echo $GIST_TOKEN > ~/.gist
+gist -u https://gist.github.com/30d835ae16e2a30efde8a63acf03628d data/report.csv
+exit $exit_status
